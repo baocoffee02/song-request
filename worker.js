39e8f2a52fb900667dfbbd7f41adefb1a0375315
baocoffee02 <baocoffee02@gmail.com>
2026-09-10 12:18:38 +0900

    Update worker.js

--- worker.js
+++ worker.js
@@ -23,7 +23,7 @@ export default {
         <head>
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Bảo Coffee</title>
+          <title>Bao Coffee Siheung </title>
           <style>
             body {
               margin: 0;
